3ac01a9cdaa217a25c8c8e0bfb2e1904c6bb9986
priyankacute <bhavanimurugesh2000@gmail.com>
2023-10-09 14:42:58 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,24 @@
+var a=[];
+var n=parseInt(prompt("Enter the size of the array"));
+
+	
+for (let i=0; i<n; i++){
+	a[i]=parseInt(prompt("Enter the" + (i+1) +" value"));
+}
+
+document.write("Given Array = "+a+"<br><br>");
+
+var b=[];
+var temp
+
+for (let i=0; i<a.length;  i++){
+	for (let j=i+1; j<a.length; j++){
+		if(a[i]<a[j]){
+			temp=a[i];
+			a[i]=a[j];
+			a[j]=temp;
+		}
+	}
+		b.push(a[i]);
+}
+document.write("the descending order of the given number is"+b)
